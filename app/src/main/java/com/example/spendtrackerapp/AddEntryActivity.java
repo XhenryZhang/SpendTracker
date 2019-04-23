e03eabd24a2293186ec6723322f4aee29f392fe8
henry <henry.zhang90@gmail.com>
2019-04-23 13:27:43 -0700

    regex error

---
+++ app/src/main/java/com/example/spendtrackerapp/AddEntryActivity.java
@@ -0,0 +1,56 @@
+package com.example.spendtrackerapp;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+import android.support.v7.widget.DialogTitle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.EditText;
+import android.widget.Toast;
+
+import java.util.regex.Matcher;
+import java.util.regex.Pattern;
+
+public class AddEntryActivity extends AppCompatActivity {
+    EditText title;
+    EditText category;
+    EditText date;
+    EditText amount;
+    String regex = "^(1[0-2]|0[1-9])/(3[01]|[12][0-9]|0[1-9])/[0-9]{4}$";
+    Pattern p = Pattern.compile(regex);
+    Matcher m;
+
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_add_entry);
+        title = (EditText) findViewById(R.id.titleEntry);
+        category = (EditText) findViewById(R.id.categoryEntry);
+        date = (EditText) findViewById(R.id.dateEntry);
+        amount = (EditText) findViewById(R.id.amountEntry);
+        m = p.matcher(date.getText().toString().trim());
+
+        Button cancel = (Button) findViewById(R.id.cancelButton);
+        cancel.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                finish();
+            }
+        });
+
+        Button save = (Button) findViewById(R.id.saveButton);
+        save.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                if (title.getText().length() == 0) {
+                    Toast.makeText(getApplicationContext(), "nice pecs bro", Toast.LENGTH_SHORT).show();
+                }else if (amount.getText().length() == 0) {
+                    Toast.makeText(getApplicationContext(), "something's wrong bro", Toast.LENGTH_SHORT).show();
+                }else if (!(m.matches())) {
+                    Toast.makeText(getApplicationContext(), "what's the time bro", Toast.LENGTH_SHORT).show();
+                }
+            }
+        });
+    }
+}
